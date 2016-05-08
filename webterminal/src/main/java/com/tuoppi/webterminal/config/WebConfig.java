f843a581f1f2b61a1c973e7f5d84d67b74fa0854
nostalgiatune <nostalgiatune@gmail.com>
2016-05-08 06:56:29 +0300

    Add InternalResourceViewResover

--- webterminal/src/main/java/com/tuoppi/webterminal/config/WebConfig.java
+++ webterminal/src/main/java/com/tuoppi/webterminal/config/WebConfig.java
@@ -1,10 +1,21 @@
 package com.tuoppi.webterminal.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 public class WebConfig {
     
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        resolver.setExposeContextBeansAsAttributes(true);
+        return resolver;
+    }
 }
---
+++ webterminal/src/main/webapp/WEB-INF/test.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : test
+    Created on : 8.5.2016, 6:55:43
+    Author     : Tuoppi
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
