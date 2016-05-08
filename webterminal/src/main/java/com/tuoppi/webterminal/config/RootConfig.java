72492614539e43ce225b489220f8aa4ba59d0057
nostalgiatune <nostalgiatune@gmail.com>
2016-05-08 06:43:51 +0300

    Bootstrap Spring MVC

---
+++ webterminal/src/main/java/com/tuoppi/webterminal/config/RootConfig.java
@@ -0,0 +1,8 @@
+package com.tuoppi.webterminal.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RootConfig {
+    
+}
---
+++ webterminal/src/main/java/com/tuoppi/webterminal/config/SpringInitializer.java
@@ -0,0 +1,23 @@
+package com.tuoppi.webterminal.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringInitializer extends
+        AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[] { RootConfig.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[] { WebConfig.class };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+    
+}
